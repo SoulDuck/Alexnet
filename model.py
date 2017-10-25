ff3616ac90cb6a3e1576e6fe4cc47c744d43efea
kimseongjung <plznw4me@naver.com>
2017-10-25 15:33:27 +0900

    add conv relu

--- model.py
+++ model.py
@@ -80,7 +80,7 @@ def build_graph(x_ , y_ , is_training):
 
     conv_out_features=[32,64,64,64,128]
     conv_kernel_sizes=[7,5,5,3,3]
-    conv_strides=[1,1,1,1,1]
+    conv_strides=[2,2,2,1,1]
     before_act_bn_mode = []
     after_act_bn_mode = []
     allow_max_pool_indices=[0,1,4]
