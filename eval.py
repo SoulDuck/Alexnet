36fd4bc2ea0803900503da536ee727eb10aebdf3
kimseongjung <plznw4me@naver.com>
2017-10-31 16:41:46 +0900

    tf.saver.max_to_keep = 10000000

--- eval.py
+++ eval.py
@@ -15,7 +15,7 @@ test_imgs = mnist.test.images.reshape([-1,28,28,1])
 test_labs = mnist.test.labels
 """
 #for Fundus_300
-train_images, train_labels, train_filenames, test_images, test_labels, test_filenames=fundus.type1('./fundus_300_debug' , resize=(288,288))
+train_images, train_labels, train_filenames, test_images, test_labels, test_filenames=fundus.type1('./fundus_300_debug' , resize=(299,299))
 
 
 def eval(model_path ,test_images):
@@ -24,13 +24,13 @@ def eval(model_path ,test_images):
     if np.max(test_images) > 1:
         test_images = test_images / 255.
     sess = tf.Session()
-    saver = tf.train.import_meta_graph(meta_graph_or_file='./models/fundus_300/.meta') #example model path ./models/fundus_300/5/model_1.ckpt
+    saver = tf.train.import_meta_graph(meta_graph_or_file=model_path+'.meta') #example model path ./models/fundus_300/5/model_1.ckpt
     saver.restore(sess, save_path=model_path) # example model path ./models/fundus_300/5/model_1.ckpt
 
     x_ = tf.get_default_graph().get_tensor_by_name('x_:0')
     y_ = tf.get_default_graph().get_tensor_by_name('y_:0')
     pred_ = tf.get_default_graph().get_tensor_by_name('softmax:0')
-    is_training_=tf.get_default_graph().get_tensor_by_name('is_training_:0')
+    is_training_=tf.get_default_graph().get_tensor_by_name('is_training:0')
     top_conv = tf.get_default_graph().get_tensor_by_name('top_conv:0')
     logits = tf.get_default_graph().get_tensor_by_name('logits:0')
 
@@ -48,9 +48,10 @@ def eval(model_path ,test_images):
     except Exception as e :
         print e
         pass
-    pred = sess.run(pred_ , feed_dict={x_ : test_images[:1],is_training_:False})
+    pred_ = sess.run(pred_ , feed_dict={x_ : test_images[:1],is_training_:False})
     print pred_
 if __name__ =='__main__':
-    model_path ='./model/fundus_300/0'
-    eval('/models/fundus_300/model_9000.ckpt', test_images)
+    model_path ='./models/fundus_0/model'
+    pred=eval(model_path, test_images)
+    print np.shape(pred)
 
